1962c984e992caf9098218b95c769555b11cd2e2
Sameer Yadav <sameer@Sameers-MacBook-Air.local>
2023-09-19 09:43:49 +0530

    used parseInt while calculating invoice value

--- frontend/src/schema/FeedbackSchema.js
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-export const validationSchema = yup.object({
-  title: yup.string("Enter title").required("Please enter a title"),
-  description: yup
-    .string("Enter a description")
-    .required("Description is required"),
-});
--- frontend/src/utils/convertDateFormatForDB.js
+++
@@ -1,5 +0,0 @@
-export function convertDateFormatForDB(dateString) {
-  const [year, month, day] = dateString?.split("-");
-  const formattedDate = `${day}.${month}.${year.slice(-2)}`;
-  return formattedDate;
-}
