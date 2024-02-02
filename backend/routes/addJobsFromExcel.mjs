1459dee29f1fa8412269d06c178a0af061b6bbb6
Sameer Yadav <sameer@Sameers-MacBook-Air.local>
2024-02-02 22:34:37 +0530

    fixed jobs setting as pending on uploading excel

--- backend/routes/addJobsFromExcel.mjs
+++ backend/routes/addJobsFromExcel.mjs
@@ -1,54 +1,3 @@
-// import express from "express";
-// import JobModel from "../models/jobModel.mjs";
-// const router = express.Router();
-// router.post("/api/jobs/addJob", async (req, res) => {
-//   const jsonData = req.body;
-
-//   try {
-//     // Extract the unique identifiers (year and job_no) from the documents
-//     const uniqueIdentifiers = jsonData.map((data) => ({
-//       year: data.year,
-//       job_no: data.job_no,
-//     }));
-
-//     // Use bulkWrite to perform the insert or update in a batch
-//     const bulkOperations = uniqueIdentifiers.map((identifier) => {
-//       const dataToUpdate = jsonData.find(
-//         (data) =>
-//           data.year === identifier.year && data.job_no === identifier.job_no
-//       );
-//       // Set the status field based on the bill_date value
-//       const status =
-//         dataToUpdate.bill_date === "" || dataToUpdate.bill_date === "--"
-//           ? "Pending"
-//           : "Completed";
-
-//       return {
-//         updateOne: {
-//           filter: identifier,
-//           update: {
-//             $set: {
-//               ...dataToUpdate,
-//               status: status,
-//             },
-//           },
-//           upsert: true, // Create if it doesn't exist
-//         },
-//       };
-//     });
-
-//     // Execute the bulkWrite operation
-//     await JobModel.bulkWrite(bulkOperations);
-
-//     res.status(200).json({ message: "Jobs added successfully" });
-//   } catch (error) {
-//     console.error("Error:", error);
-//     return res.status(500).json({ error: "Internal server error." });
-//   }
-// });
-
-// export default router;
-
 import express from "express";
 import JobModel from "../models/jobModel.mjs";
 
@@ -77,24 +26,27 @@ router.post("/api/jobs/addJob", async (req, res) => {
         data.vessel_berthing_date = existingJob.vessel_berthing_date;
       }
 
-      // Define the update to set new data, including "container_nos"
-      const update = {
-        $set: {
-          ...data,
-          status: computeStatus(data.bill_date),
-        },
-      };
+      // If the existing status is already "Completed," skip updating the status
+      if (!(existingJob && existingJob.status === "Completed")) {
+        // Define the update to set new data, including "container_nos"
+        const update = {
+          $set: {
+            ...data,
+            status: computeStatus(data.bill_date),
+          },
+        };
 
-      // Create the bulk update operation for upsert or update
-      const bulkOperation = {
-        updateOne: {
-          filter,
-          update,
-          upsert: true, // Create if it doesn't exist
-        },
-      };
+        // Create the bulk update operation for upsert or update
+        const bulkOperation = {
+          updateOne: {
+            filter,
+            update,
+            upsert: true, // Create if it doesn't exist
+          },
+        };
 
-      bulkOperations.push(bulkOperation);
+        bulkOperations.push(bulkOperation);
+      }
     }
 
     // Execute the bulkWrite operation to update or insert multiple jobs
