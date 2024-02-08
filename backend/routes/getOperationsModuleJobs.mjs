119ee2412a74c4bc7d582eac8d90b3e5419f72f9
Sameer Yadav <sameer@Sameers-MacBook-Air.local>
2024-02-08 14:21:02 +0530

    filtering jobs in ops module based on examination planning date

--- backend/routes/getOperationsModuleJobs.mjs
+++ backend/routes/getOperationsModuleJobs.mjs
@@ -1,44 +1,3 @@
-// import express from "express";
-// import JobModel from "../models/jobModel.mjs";
-// import User from "../models/userModel.mjs";
-
-// const router = express.Router();
-
-// router.get("/api/getOperationsModuleJobs/:email", async (req, res) => {
-//   const { email } = req.params;
-//   const user = await User.findOne({ email });
-
-//   if (!user) {
-//     return res.status(404).send("User not found");
-//   }
-
-//   const port = user.port;
-
-//   let additionalCondition = {};
-
-//   if (
-//     email !== "mahesh@surajforwarders.com" &&
-//     email !== "paras@surajforwarders.com" &&
-//     email !== "operations@surajforwarders.com"
-//   ) {
-//     additionalCondition = {
-//       custom_house: { $in: port },
-//     };
-//   }
-
-//   const jobs = await JobModel.find(
-//     {
-//       examinationPlanning: "true",
-//       ...additionalCondition,
-//     },
-//     "job_no be_no be_date container_nos"
-//   );
-
-//   res.status(200).send(jobs);
-// });
-
-// export default router;
-
 import express from "express";
 import JobModel from "../models/jobModel.mjs";
 import User from "../models/userModel.mjs";
@@ -54,17 +13,18 @@ router.get("/api/getOperationsModuleJobs/:email", async (req, res) => {
   }
 
   const port = user.port;
+  const currentDate = new Date().toISOString().split("T")[0]; // Get current date in "yyyy-mm-dd" format
 
-  let additionalCondition = {};
+  let additionalCondition = {
+    examination_planning_date: { $gte: currentDate },
+  };
 
   if (
     email !== "mahesh@surajforwarders.com" &&
     email !== "paras@surajforwarders.com" &&
     email !== "operations@surajforwarders.com"
   ) {
-    additionalCondition = {
-      custom_house: { $in: port },
-    };
+    additionalCondition.custom_house = { $in: port };
   }
 
   const jobs = await JobModel.find(
